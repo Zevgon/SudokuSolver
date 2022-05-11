075fa69054695ace8168363d754d1500c4ada87d
Yale Work <yale.work@gmail.com>
2022-05-11 01:22:42 -0600

    maybe working

--- solution.py
+++ solution.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from itertools import combinations
 from math import sqrt
 
@@ -21,41 +22,51 @@ class Node:
     
     def add_group(self, group):
         self.groups.add(group)
+    
+    def __str__(self):
+        return str(list(self.possibilities)[0])
+
+class GroupType(Enum):
+    ROW = 1,
+    COLUMN = 2,
+    BOX = 3
 
 # Contains 9 nodes, and represents a row/column/box
 class Group:
-    def __init__(self, nodes):
+    def __init__(self, nodes, group_type):
         self.nodes = nodes
+        self.group_type = group_type
         for node in nodes:
             node.add_group(self)
 
-    # There's a trim rule that needs to be added: if rows 1 and 4 contain a 2 in the
-    # possibilities for columns 1 and 2, and don't include a 2 anywhere else in the
-    # possibilities for those rows, 2 needs to be removed from the possibilities of
-    # all other squares in columns 1 and 2
     def trim_possibilities(self):
         for num in range(1, len(self.nodes) + 1):
             nodes_with_num = [n for n in self.nodes if num in n.possibilities]
             other_groups = [g for g in combine_groups(nodes_with_num) if g != self]
             for group in other_groups:
                 if all([n in group.nodes for n in nodes_with_num]):
-                    group.remove_possibility_from_other_nodes(nodes_with_num, num)
+                    group.remove_possibilities_from_other_nodes(nodes_with_num, [num])
 
-        for num_possibilities in range(1, len(self.nodes)):
-            for comb in combinations(self.nodes, num_possibilities):
+        for num_nodes in range(1, len(self.nodes)):
+            for comb in combinations(self.nodes, num_nodes):
                 all_possibilities = combine_possibilities(comb)
-                if len(all_possibilities) == num_possibilities:
+                if len(all_possibilities) == num_nodes:
                     other_nodes = self.nodes.difference(comb)
                     for other_node in other_nodes:
                         other_node.possibilities = other_node.possibilities.difference(all_possibilities)
 
-    def remove_possibility_from_other_nodes(self, nodes, num):
+    def remove_possibilities_from_other_nodes(self, nodes, nums):
         for node in self.nodes.difference(nodes):
-            if num in node.possibilities:
-                node.possibilities.remove(num)
+            node.possibilities = node.possibilities.difference(nums)
     
     def is_solved(self):
         return all([len(node.possibilities) == 1 for node in self.nodes])
+    
+    def has_x_or_fewer_nodes_with_num(self, num_nodes, num):
+        return len(self.get_nodes_with_num(num)) <= num_nodes
+    
+    def get_nodes_with_num(self, num):
+        return set([node for node in self.nodes if num in node.possibilities])
 
 # nodes = [
 #     Node(set([1, 2])),
@@ -105,10 +116,11 @@ class Group:
 #     Node(set([1, 3, 6, 7])),
 # ]
 
-# g = Group(set(nodes))
+# g = Group(set(nodes), GroupType.ROW)
 # g.trim_possibilities()
 # print([node.possibilities for node in g.nodes])
 
+# easy for the algo
 # board = [
 #     ["5","3",".",".","7",".",".",".","."],
 #     ["6",".",".","1","9","5",".",".","."],
@@ -120,6 +132,7 @@ class Group:
 #     [".",".",".","4","1","9",".",".","5"],
 #     [".",".",".",".","8",".",".","7","9"]]
 
+# harder for the algo (failing leetcode test case after first attempt)
 board = [
     [".",".","9","7","4","8",".",".","."],
     ["7",".",".",".",".",".",".",".","."],
@@ -130,6 +143,29 @@ board = [
     [".",".",".","8",".","3",".","2","."],
     [".",".",".",".",".",".",".",".","6"],
     [".",".",".","2","7","5","9",".","."]]
+
+# board = [
+#     [".",".",".",".",".",".",".",".","."],
+#     [".",".",".",".",".",".",".",".","."],
+#     [".",".",".",".",".",".",".",".","."],
+#     [".",".",".",".",".",".",".",".","."],
+#     [".",".",".",".",".",".",".",".","."],
+#     [".",".",".",".",".",".",".",".","."],
+#     [".",".",".",".",".",".",".",".","."],
+#     [".",".",".",".",".",".",".",".","."],
+#     [".",".",".",".",".",".",".",".","."]]
+
+# fail
+# board = [
+#     [".",".",".",".",".",".",".",".","."],
+#     [".",".",".",".",".",".",".",".","."],
+#     [".",".",".",".",".",".","2",".","."],
+#     [".",".",".",".",".",".",".",".","."],
+#     [".",".",".",".",".",".",".",".","."],
+#     [".",".",".",".","2",".",".","2","."],
+#     [".",".",".",".",".",".",".",".","."],
+#     [".",".",".",".",".",".",".",".","."],
+#     [".",".","2",".",".","2",".",".","2"]]
 
 def create_node_matrix(sudoku):
     node_matrix = [[None for _ in range(len(sudoku))] for _ in range(len(sudoku))]
@@ -147,13 +183,13 @@ def create_groups(node_matrix):
         row = []
         for j in range(len(node_matrix[i])):
             row.append(node_matrix[i][j])
-        groups.append(Group(set(row)))
+        groups.append(Group(set(row), GroupType.ROW))
 
     for j in range(len(node_matrix[0])):
         column = []
         for i in range(len(node_matrix)):
             column.append(node_matrix[i][j])
-        groups.append(Group(set(column)))
+        groups.append(Group(set(column), GroupType.COLUMN))
     
     boxes = [[] for _ in range(len(node_matrix))]
     box_size = int(sqrt(len(node_matrix)))
@@ -162,7 +198,7 @@ def create_groups(node_matrix):
             box_idx = i // box_size * box_size + j // box_size
             boxes[box_idx].append(node_matrix[i][j])
     for box in boxes:
-        groups.append(Group(set(box)))
+        groups.append(Group(set(box), GroupType.BOX))
     
     return groups
 
@@ -175,21 +211,78 @@ def convert_node_matrix_to_str_matrix(node_matrix):
             if len(possibilities) > 1:
                 row.append('.')
             else:
-                row.append(str(possibilities[0]))
+                # TODO remove
+                if len(possibilities):
+                    row.append(str(possibilities[0]))
+                else:
+                    row.append('0')
         res.append(row)
     return res
+
+def get_nodes_with_num(groups, num):
+    nodes = set()
+    for group in groups:
+        nodes = nodes.union(group.get_nodes_with_num(num))
+    return nodes
+
+def get_all_groups_with_node_with_num(groups, num, group_type):
+    columns = set()
+    for group in groups:
+        nodes_with_num = group.get_nodes_with_num(num)
+        for node in nodes_with_num:
+            for node_group in node.groups:
+                if node_group.group_type == group_type:
+                    columns.add(node_group)
+    return columns
+
+# This function handles situations like the following:
+# 
+# R1 possibilities:     [2, ...], [2, ...], [...], [2, ...], [  ... ], [...], [...], [...], [...]
+# R2 possibilities:     [2, ...], [  ... ], [...], [2, ...], [  ... ], [...], [...], [...], [...]
+# R3 possibilities:     [2, ...], [2, ...], [...], [2, ...], [  ... ], [...], [...], [...], [...]
+# R4 possibilities:     [2, ...], [2, ...], [...], [  ... ], [2, ...], [...], [...], [...], [...]
+#
+# In row 4, the 2s in columns 1 and 2 should be trimmed from the possibilities because a 2 must go
+# in one of the 3 spots in row 1, one of the 2 spots in row 2, and one of the 3 spots in row 3.
+# In every case, a 2 must appear in column 1 and column 2 within the top 3 rows, meaning it can't
+# appear in column 1 or 2 in row 4.
+
+def trim_by_group_sets(rows, columns):
+    for num in range(1, len(rows) + 1):
+        for num_nodes in range(2, len(rows)):
+            rows_with_x_or_fewer_nodes_with_num = [group for group in rows if group.has_x_or_fewer_nodes_with_num(num_nodes, num)]
+            nodes_with_num = get_nodes_with_num(rows_with_x_or_fewer_nodes_with_num, num)
+            all_columns = get_all_groups_with_node_with_num(rows_with_x_or_fewer_nodes_with_num, num, GroupType.COLUMN)
+            if len(all_columns) == len(rows_with_x_or_fewer_nodes_with_num):
+                for column in all_columns:
+                    column.remove_possibilities_from_other_nodes(nodes_with_num, [num])
+
+    for num in range(1, len(rows) + 1):
+        for num_nodes in range(2, len(rows)):
+            columns_with_x_or_fewer_nodes_with_num = [group for group in columns if group.has_x_or_fewer_nodes_with_num(num_nodes, num)]
+            nodes_with_num = get_nodes_with_num(columns_with_x_or_fewer_nodes_with_num, num)
+            all_rows = get_all_groups_with_node_with_num(columns_with_x_or_fewer_nodes_with_num, num, GroupType.ROW)
+            if len(all_rows) == len(columns_with_x_or_fewer_nodes_with_num):
+                for row in all_rows:
+                    row.remove_possibilities_from_other_nodes(nodes_with_num, [num])
 
 def solve(sudoku):
     node_matrix = create_node_matrix(sudoku)
     groups = create_groups(node_matrix)
+    rows = groups[0:9]
+    columns = groups[9:18]
 
     while any([not group.is_solved() for group in groups]):
         for unsolved_group in [group for group in groups if not group.is_solved()]:
             unsolved_group.trim_possibilities()
+        
+        trim_by_group_sets(rows, columns)
+
         for row in convert_node_matrix_to_str_matrix(node_matrix):
             print(row)
         print('')
         print('')
+        # return
     
     return convert_node_matrix_to_str_matrix(node_matrix)
 
